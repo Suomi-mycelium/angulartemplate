d355f45859f335ece4bef004d79a093309a2beaa
Abner García <abnerjosgarfer@yahoo.com>
2025-07-09 22:18:20 -0600

    Logic to go to next question added

--- src/app/learn/game/game.ts
+++ src/app/learn/game/game.ts
@@ -10,17 +10,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './game.scss'
 })
 export class Game implements OnInit {
-  private questionService = inject(Questions);
-  activeQuestionId = 0;
+  private questionService: Questions = inject(Questions);
+  activeQuestionId: number = 0;
+  finished: boolean = false;
   questions$!: Observable<Question[]>;
 
   ngOnInit() {
     this.questions$ = this.questionService.getQuestions();
   }
 
-  checkAnswer(question: Question, selectedOption: string) {
+  checkAnswer(question: Question, selectedOption: string, lastQuestion: boolean) {
     if (selectedOption === question.answer) {
       alert("Correct!");
+      if (lastQuestion) {
+        alert("Finished!");
+      } else {
+        this.activeQuestionId++;
+      }
     } else {
       alert("Incorrect!");
     }
